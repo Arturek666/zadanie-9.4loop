2dd0d4dfec7f9c0326e13c3715e807678d268e1a
Artur Schulz <artur.schulz123@gmail.com>
2018-03-02 16:51:52 +0100

    Add files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+function drawTree(x) {
+	for ( var i = 0 ; i <= x ; i++) {
+var star = '';
+
+	for ( var j = 0 ; j <= i ; j++) {
+		star += '*';
+}
+console.log(star);
+
+}
+}
+console.log(drawTree(5));
